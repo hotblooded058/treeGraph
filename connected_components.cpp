a166c2faf3e317e4d6e9baa114c9d8d97d7644f3
hotblooded058 <nightmareisalone@gmail.com>
2022-05-25 12:25:53 +0530

    2

---
+++ connected_components.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+
+vector<int>g[N];
+bool vit[N];
+
+void dfs(int vertex)
+{
+    vit[vertex]=true;
+    for(int child: g[vertex])
+    {
+        if(vit[child])
+        {
+            continue;
+        }
+        dfs(child);
+    }
+}
+
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+
+    for(int i=0;i<e;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+
+        g[y].push_back(x);
+        g[x].push_back(y);
+    }
+    int cnt=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(vit[i])
+        {
+            continue;
+        }
+        dfs(i);
+        cnt++;
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
