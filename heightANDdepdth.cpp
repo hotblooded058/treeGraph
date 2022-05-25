c1ddf649652f9af64abcd61b25ac6ed3ca457efe
hotblooded058 <nightmareisalone@gmail.com>
2022-05-25 18:30:54 +0530

    height & depth

---
+++ heightANDdepdth.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+vector<int>g[N];
+int depth[N],height[N];
+void dfs(int vertex,int par=0)
+{
+
+    for(int child:g[vertex])
+    {
+        if(child==par) continue;
+        depth[child]=depth[vertex]+1;
+        dfs(child,vertex);
+        height[child]=max(height[vertex],height[child]+1);
+
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    dfs(1);
+    return 0;
+}
